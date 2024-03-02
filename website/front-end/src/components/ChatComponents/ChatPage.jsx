a78427b5df934d21eeeaeda21c02265122b43dc1
WiBuMoney <cowsmile1911@gmail.com>
2024-03-02 13:00:22 +0700

    *Cập nhật: - Chat với nhiều loại bot

--- website/front-end/src/components/ChatComponents/ChatPage.jsx
+++ website/front-end/src/components/ChatComponents/ChatPage.jsx
@@ -1,11 +1,19 @@
 import Navigation from '../HomeComponents/Navigation';
 import Body from './Chat.Body';
 
-const ChatPage = ({ messages, setMessages, chatAble, setChatAble, fixedData }) => {
+const ChatPage = ({ messages, setMessages, chatAble, setChatAble, fixedData, botType, setBotType, handleBotType }) => {
 	return (
 		<>
 			<Navigation data={fixedData.Header} />
-			<Body messages={messages} setMessages={setMessages} chatAble={chatAble} setChatAble={setChatAble} />
+			<Body
+				messages={messages}
+				setMessages={setMessages}
+				chatAble={chatAble}
+				setChatAble={setChatAble}
+				botType={botType}
+				setBotType={setBotType}
+				handleBotType={handleBotType}
+			/>
 		</>
 	);
 };
